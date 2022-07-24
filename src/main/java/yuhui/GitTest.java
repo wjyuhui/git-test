5352ba3b795f6560ca0b759084ab7fec01c6fb86
yuhui <a2273090901@163.com>
2022-07-25 00:37:09 +0800

    hot-fix second commit

--- src/main/java/yuhui/GitTest.java
+++ src/main/java/yuhui/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("版本2");
         System.out.println("版本3");
         System.out.println("版本4");
+        System.out.println("hot-fix test!");
     }
 }
