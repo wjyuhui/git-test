45f56b9c7e489d38f53ab411b3f6a991fca58d15
yuhui <a2273090901@163.com>
2022-07-25 00:17:42 +0800

    thridly commit

--- src/main/java/yuhui/GitTest.java
+++ src/main/java/yuhui/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("版本2");
+        System.out.println("版本3");
     }
 }
